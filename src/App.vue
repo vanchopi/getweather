702bfc49e2a6ecd15418763bf667b2ee28ebc6f9
vanchopi <ivan.ugnenko@webregul.ru>
2018-12-27 04:13:14 +0300

    vue init

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container pt-5">      
+      <div class="row">
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text" id="inputGroup-sizing-default">City</span>
+          </div>
+          <input type="search" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+        </div>  
+      </div>
+      <div class="mb-3">
+        <div class="row">
+          <button type="button" class="btn btn-primary" @click="getData">Get</button>
+        </div>  
+      </div>
+      <span>
+        {{wether}}
+      </span>
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  
+  data () {
+    return {
+      name: '',
+      wether: []
+    }
+  },
+  methods:{
+    getData(){
+      this.resource.get().then(response => response.json())
+          .then(wether => this.wether = wether)      
+    }
+  },
+  created(){
+    this.resource = this.$resource('42.3601,-71.0589')
+  }
+}
+</script>
+
+<style type="text/css" scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+
+Vue.use(VueRouter)
+
+Vue.use(VueResource)
+
+Vue.http.options.root = 'https://api.darksky.net/forecast/53dbf25ba49514eff6ccd794e04da9ae/'
+
+Vue.http.interceptors.push(request => {
+	/*request.headers.set('Access-Control-Allow-Origin', 'http://localhost:8080')
+	request.headers.set('Access-Control-Allow-Credentials', 'true')*/
+})
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
