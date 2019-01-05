0f78c2b9697208d3ebfe2ec362575ea61da65934
vanchopi <ivan.ugnenko@webregul.ru>
2019-01-05 03:13:23 +0300

    error image

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,11 +1,22 @@
 <template>
-	<div>
-		404
+	<div class="row text-center">
+		<div class="img-cont">
+			<img src="/src/assets/images/error.png">	
+		</div>	
 	</div>
 </template>
 
 <script type="text/javascript">
 </script>
 
-<style type="text/css">	
+<style type="text/css" scoped>	
+	.row .img-cont{
+		width: 145px;
+		margin: auto;
+	}
+
+	.row .img-cont img{
+		width: 100%;
+	}
+
 </style>	
